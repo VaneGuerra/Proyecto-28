f6b4170327dcc1b97413b0ec73b242ab3b9c5ed3
VaneGuerra <101012800+VaneGuerra@users.noreply.github.com>
2022-04-22 13:10:22 -0500

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,25 @@
+class Launcher {
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.Launcher = Constraint.create(options);
+        this.pointB = pointB;
+        World.add(world, this.Launcher);
+    }
+
+    display(){
+        if(this.Launcher.bodyA){
+            var pointA = this.Launcher.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    fly(){
+        this.Launcher.bodyA = null;
+    }
+}
